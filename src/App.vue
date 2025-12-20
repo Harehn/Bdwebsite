c24295ffc63b5945f940a432dfb4c5a679875070
Nitin <harehn.kaundun@mail.mcgill.ca>
2025-12-19 21:50:42 -0500

    Added box container

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <Box style="background-color:aqua"><h1>Happy birthday, Johanna!! 🎂</h1></Box>
+    <Box style="background-color:grey">Animation</Box>
+    <Box style="background-color:blue">Letter</Box>
+    <Box style="background-color:black">Text Message</Box>
+    <Box style="background-color:red">Masonry wall</Box>
+    <Box style="background-color:purple">Song</Box>
+  </div>
 </template>
 
 <script>
+import Box from './components/Box.vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Box
   }
 }
 </script>
 
+
 <style>
+html, body {
+  height:100%;
+  width: 100%;
+  margin: 0px;
+  padding: 0px;
+  overflow-x: hidden; 
+
+  /* margin: 0;
+  display:flex;
+  flex-direction: column;
+  justify-content: space-between; */
+}
 #app {
+  height:100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* scroll-snap-type: y mandatory; */
+  /* margin-top: 60px; */
+}.main > div {
+  scroll-snap-align: start;
+}
+.main {
+  height:100%;
+  width: 100%;
+  scroll-snap-type: y mandatory;
+  overflow-y:scroll;
 }
 </style>
